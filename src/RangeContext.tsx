2643da5cbec24afa42ae06509a8179f30db2c4bd
刘宇萱 <yuxuan.liu@nascent.cn>
2021-09-28 14:43:18 +0800

    first commit

---
+++ src/RangeContext.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import type { NullableDateType, RangeValue } from './interface';
+
+export type RangeContextProps = {
+  /**
+   * Set displayed range value style.
+   * Panel only has one value, this is only style effect.
+   */
+  rangedValue?: [NullableDateType<any>, NullableDateType<any>] | null;
+  hoverRangedValue?: RangeValue<any>;
+  inRange?: boolean;
+  panelPosition?: 'left' | 'right' | false;
+  currentPositionIndex?: any;
+};
+
+const RangeContext = React.createContext<RangeContextProps>({});
+
+export default RangeContext;
